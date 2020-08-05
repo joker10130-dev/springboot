7f476a177069cdc699d88836e49d07ab47687bff
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-08-05 16:31:57 +0700

    step_05 add pagination function

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -21,17 +23,19 @@ public class UserController {
         user.setName(request.getName());
         user.setAge(request.getAge());
         user = userRepository.save(user);
-        return new UsersResponse(user.getId(),user.getName()+user.getAge());
+        return new UsersResponse(user.getId(),user.getName(), user.getAge());
     }
 
+//    http://localhost:8080/users?page=0&item_per_page=10
     @GetMapping("/users")
-    public PagingResponse getAllUser(@RequestParam(defaultValue ="1") int page, @RequestParam(name="item_per_page",defaultValue = "10") int itemPerPage){
+    public PagingResponse getAllUser(@RequestParam(defaultValue ="0") int page, @RequestParam(name="item_per_page",defaultValue = "10") int itemPerPage){
         PagingResponse pr = new PagingResponse(page, itemPerPage);
         List<UsersResponse> usersResponseList = new ArrayList<>();
+        Pageable pageable = PageRequest.of(page,itemPerPage);
 
-        Iterable<User> users = userRepository.findAll();
+        Iterable<User> users = userRepository.findAll(pageable);
         for(User user: users){
-            usersResponseList.add(new UsersResponse(user.getId(),user.getName()));
+            usersResponseList.add(new UsersResponse(user.getId(),user.getName(),user.getAge()));
         }
 
         pr.setUsers(usersResponseList);
--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
 
+//public interface UserRepository extends CrudRepository<User, Integer> {
+//
+//}
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
+    Page<User> findAll(Pageable pageable);
 }
