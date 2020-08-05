45aa20485b8c2f47bb2a4eac58ad32910ff8d559
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-08-05 11:59:40 +0700

    step02 and add another method for test only

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 ### Test
 $gradlew
+
 $gradlew test
+
 $gradlew bootRun
 
 ### Production
 $gradlew bootJar
+
 $gradlew clean bootJar
+
 $java -jar build\libs\demo-0.0.1-SNAPSHOT.jar
 
 spring boot actuator
--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,16 +1,31 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UsersResponse[] getAllUser(){
-        UsersResponse[] u = new UsersResponse[2];
-        u[0] = new UsersResponse(1, "User 1");
-        u[1] = new UsersResponse(2, "User 2");
-        return u;
+    @ResponseBody
+    public List<UsersResponse> getAllUser(@RequestParam(required = false, defaultValue ="2") int page, @RequestParam(required = false, defaultValue = "15") int item){
+        List<UsersResponse> users = new ArrayList<>();
+        users.add(new UsersResponse(1, "User 1"));
+        users.add(new UsersResponse(2, "User 2"));
+        users.add(new UsersResponse(3, "User 3"));
+        return users;
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String getTest(@RequestParam(required = false, defaultValue ="2") int page, @RequestParam(required = false, defaultValue = "15") int item){
+        return "page "+page+", and "+ item+" item per page";
+    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id){
+        return new UsersResponse(id, "User "+id);
     }
 }
